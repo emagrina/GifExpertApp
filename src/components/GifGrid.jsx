c8de28fcd023ac1e8d9f78ad7d39d6b5e7329ff7
Ernest Magriñá <ernest8081@gmail.com>
2023-04-30 06:37:36 +0200

    Update GifGrid.jsx

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -15,22 +15,27 @@ export const GifGrid = ( { category } ) => {
         } );
     };
 
-    return ( <>
+    return ( 
+    <>
         <h3 className="m-lg-3">{ category }</h3>
         { isLoading && ( <div className="lds-ripple">
             <div></div>
             <div></div>
         </div> ) }
         <div className="container gif-results">
-            { images.map( ( { id, url, title } ) => ( <GifItem
-                key={ id }
-                id={ id }
-                url={ url }
-                title={ title }
-                copyToClipboard={ copyToClipboard }
-                showToast={ showToast }
-                setShowToast={ setShowToast }
-            /> ) ) }
+            { 
+                images.map(( { id, url, title } ) => ( 
+                    <GifItem
+                        key={ id }
+                        id={ id }
+                        url={ url }
+                        title={ title }
+                        copyToClipboard={ copyToClipboard }
+                        showToast={ showToast }
+                        setShowToast={ setShowToast }
+                    /> 
+                )) 
+            }
         </div>
         <CopiedToast showToast={ showToast } onClose={ () => setShowToast( false ) }/>
 
